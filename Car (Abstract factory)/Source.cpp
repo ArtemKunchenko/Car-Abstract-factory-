2290f56fddbdd02a5928dd1fd9781f8d6e292c36
Артем Кунченко <kunchenko911@ukr.net>
2023-09-06 22:51:20 +0300

    Added class wheel, Hood, Door

--- Car (Abstract factory)/Source.cpp
+++ Car (Abstract factory)/Source.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
 
-
-
-
+class Wheel
+{
+public:
+	virtual void wheel() = 0;
+};
+class WheelType1 :public Wheel
+{
+	void wheel() override { cout << "Wheel type 1" << endl; }
+};
+class WheelType2 :public Wheel
+{
+	void wheel() override { cout << "Wheel type 2" << endl; }
+};
+class WheelType3 :public Wheel
+{
+	void wheel() override { cout << "Wheel type 3" << endl; }
+};
 
 int main()
 {
